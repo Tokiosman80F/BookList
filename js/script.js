5374ba2bfc1b90628e7b8a92e2b7f0aea060627f
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2021-07-31 13:47:41 +0600

    importing the input from form to table

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // get ul element
 
 let form=document.querySelector('#book-form');
+
+
 //book class
 class Book
 {
@@ -12,6 +14,40 @@ class Book
     }
 }
 
+//UI class
+class UI
+{
+    contructor()
+    {
+
+    }
+    addToBookList(book)
+    {
+    
+        let list=document.querySelector('#book-list');
+
+        let row=document.createElement('tr');
+        row.innerHTML=`
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href='#' class="delete">X</a></td>`
+        
+        list.appendChild(row);
+        
+        console.log(row);
+    }
+    
+    clearFields()
+    {
+        document.querySelector('#title').value='';
+        document.querySelector('#author').value='';
+        document.querySelector('#isbn').value='';
+        
+    }
+}
+
+
 // Event Listener
 
 form.addEventListener('submit',newBook);
@@ -22,6 +58,27 @@ form.addEventListener('submit',newBook);
 
 function newBook(e)
 {
-    console.log("hello");
+    let title=document.querySelector('#title').value;
+    let author=document.querySelector('#author').value;
+    let isbn=document.querySelector('#isbn').value;
+
+    if(title===''|| author===''|| isbn==='' )
+    {
+        alert('Fleid are empty..Please fillup the form! ')
+    }
+
+    else
+    {
+
+        let book=new Book(title,author,isbn);//This book
+    
+        let ui=new UI();//calling ui class inside new ui variable 
+        ui.addToBookList(book) ;///here the book variable is called .... calling the function addToBookList
+
+        ui.clearFields()
+    }
+
+    
+
     e.preventDefault();
 }
